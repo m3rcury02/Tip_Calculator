b3c541f6d1b65ceb553920e7e31b5abae66504e7
m3rcury02 <gunal2002@gmail.com>
2021-05-06 20:36:27 +0530

    changes

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.tipcalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calcButton.setOnClickListener { calcTip() }
+    }
+
+    private fun calcTip() {
+        val costText = binding.costOfService.text.toString()
+        val cost = costText.toFloatOrNull()
+                ?: return Toast.makeText(this, "Please enter the Cost of Service!", Toast.LENGTH_SHORT).show()
+        val tipPercent = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.fifteen_percent -> 0.15
+            R.id.twelve_percent -> 0.12
+            else -> 0.10
+        }
+        var tip = tipPercent * cost
+        if(binding.roundupSwitch.isChecked){
+            tip= kotlin.math.ceil(tip)
+        }
+        val tipFormatted = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, tipFormatted)
     }
 }
